90c078bf99d43b825add5ea1d19365e36ec0c6d7
nikhil karkra <karkra.nikhil@gmail.com>
2020-01-28 22:18:19 +1100

    redis setup

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,4 +1,10 @@
+
 const fetch = require('node-fetch');
+const redis = require('redis')
+const client = redis.createClient();
+const {promisify} = require('util')
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 const baseURL ='https://jobs.github.com/positions.json'
 async function fetchGithub(){
     let resultCount = 1
@@ -12,6 +18,8 @@ async function fetchGithub(){
         onPage++;
     }
     console.log('Total Jobs', allJobs.length)
+    const success = await setAsync('github', JSON.stringify(allJobs))
+    console.log({success})
 }
-fetchGithub()
+//fetchGithub()
 module.exports = fetchGithub;
